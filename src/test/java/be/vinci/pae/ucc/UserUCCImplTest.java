d1909b2b5ac6065806548e865a660abc45a28c2b
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-03-06 13:38:05 +0100

    feat: add UserUCCImplTest

---
+++ src/test/java/be/vinci/pae/ucc/UserUCCImplTest.java
@@ -0,0 +1,15 @@
+package be.vinci.pae.ucc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserUCCImplTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void login() {
+  }
+}
