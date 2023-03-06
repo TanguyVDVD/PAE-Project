4bd39b81b735bcb1183c9cc769b6eb9e3cd12b8d
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-03-06 16:37:25 +0100

    feat: add connectDatabase method and constructor

--- src/main/java/be/vinci/pae/services/DalServicesImpl.java
+++ src/main/java/be/vinci/pae/services/DalServicesImpl.java
@@ -1,18 +1,54 @@
 package be.vinci.pae.services;
 
-import static be.vinci.pae.services.UserDSImpl.DB_CONNECTION;
-
+import be.vinci.pae.utils.Config;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DalServicesImpl implements DalServices {
+
+  public static final String DATABASE_URL;
+  // User connecting to the database
+  public static final String DATABASE_USER;
+  // User's password
+  public static final String DATABASE_PASSWORD;
+
+  // Connection to the database
+  public static Connection DB_CONNECTION = null;
+
+  // Loading properties
+  static {
+    Config.load("dev.properties");
+    DATABASE_URL = Config.getProperty("DatabaseUrl");
+    DATABASE_USER = Config.getProperty("DatabaseUser");
+    DATABASE_PASSWORD = Config.getProperty("DatabasePassword");
+  }
+
+  public DalServicesImpl() {
+    connectDatabase();
+  }
 
   /**
    * Load the PostgresSQL driver and connect to the database.
    */
   @Override
   public void connectDatabase() {
+    // Load the PostgresSQL driver
+    try {
+      Class.forName("org.postgresql.Driver");
+    } catch (ClassNotFoundException e) {
+      System.out.println("Missing PostgreSQL driver!");
+      System.exit(1);
+    }
 
+    // Connection to the database
+    try {
+      DB_CONNECTION = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+    } catch (SQLException e) {
+      System.out.println("Unable to reach the server!");
+      System.exit(1);
+    }
   }
 
   /**
