841395be964adf934af2ade3f95b0fbc8d65574d
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-03-06 14:21:50 +0100

    feat: set up tests

--- src/test/java/be/vinci/pae/ucc/UserUCCImplTest.java
+++ src/test/java/be/vinci/pae/ucc/UserUCCImplTest.java
@@ -1,12 +1,24 @@
 package be.vinci.pae.ucc;
 
+import be.vinci.pae.domain.User;
+import be.vinci.pae.domain.UserDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class UserUCCImplTest {
 
+  private UserUCC userUCC;
+  private UserDTO userDTO;
+  private User user;
+  private String correctEmail;
+  private String correctPassword;
+
   @BeforeEach
   void setUp() {
+    userDTO = Mockito.mock(UserDTO.class);
+    Mockito.when(userDTO.getEmail()).thenReturn(correctEmail);
+    Mockito.when(userDTO.getPassword()).thenReturn(user.hashPassword(correctPassword));
   }
 
   @Test
