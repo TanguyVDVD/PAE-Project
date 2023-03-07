3e16fff8202e8bb0cdde520589032edde887c9d1
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-03-07 10:16:11 +0100

    feat: add ObjectImpl in domain

---
+++ src/main/java/be/vinci/pae/domain/ObjectImpl.java
@@ -0,0 +1,346 @@
+package be.vinci.pae.domain;
+
+import java.util.Date;
+
+public class ObjectImpl implements Object {
+
+  /**
+   * Return the id of an object.
+   *
+   * @return an int corresponding to the id of an object
+   */
+  @Override
+  public int getId() {
+    return 0;
+  }
+
+  /**
+   * set the id of an object.
+   *
+   * @param id the id of an object
+   */
+  @Override
+  public void setId(int id) {
+
+  }
+
+  /**
+   * Return the description of an object.
+   *
+   * @return a String corresponding to the description of an object
+   */
+  @Override
+  public String getDescription() {
+    return null;
+  }
+
+  /**
+   * set the description of an object.
+   *
+   * @param description the description of an object
+   */
+  @Override
+  public void setDescription(String description) {
+
+  }
+
+  /**
+   * Return the path of the photo of an object.
+   *
+   * @return a String corresponding to the path of the photo of an object
+   */
+  @Override
+  public String getPhoto() {
+    return null;
+  }
+
+  /**
+   * set the description of an object.
+   *
+   * @param photo the path of the photo of an object
+   */
+  @Override
+  public void setPhoto(String photo) {
+
+  }
+
+  /**
+   * Return the visibility of an object.
+   *
+   * @return true if the object is visible, else false
+   */
+  @Override
+  public Boolean isVisible() {
+    return null;
+  }
+
+  /**
+   * set the visibility of an object.
+   *
+   * @param visibility the visibility of an object
+   */
+  @Override
+  public void setVisibility(boolean visibility) {
+
+  }
+
+  /**
+   * Return the price of an object.
+   *
+   * @return a double corresponding to the price of an object
+   */
+  @Override
+  public double getPrice() {
+    return 0;
+  }
+
+  /**
+   * set the price of an object.
+   *
+   * @param price the price of an object
+   */
+  @Override
+  public void setPrice(double price) {
+
+  }
+
+  /**
+   * Return the state of an object.
+   *
+   * @return a String corresponding to the state of an object
+   */
+  @Override
+  public String getState() {
+    return null;
+  }
+
+  /**
+   * set the state of an object.
+   *
+   * @param state the password of an object
+   */
+  @Override
+  public void setState(String state) {
+
+  }
+
+  /**
+   * Return the acceptance date of an object.
+   *
+   * @return a Date corresponding to the acceptance date of the object
+   */
+  @Override
+  public Date getAcceptanceDate() {
+    return null;
+  }
+
+  /**
+   * set the acceptance date of an object.
+   *
+   * @param acceptanceDate the acceptance date of an object
+   */
+  @Override
+  public void setAcceptanceDate(Date acceptanceDate) {
+
+  }
+
+  /**
+   * Return the deposit date of an object.
+   *
+   * @return a Date corresponding to the deposit date of the object
+   */
+  @Override
+  public Date getDepositDate() {
+    return null;
+  }
+
+  /**
+   * set the deposit date of an object.
+   *
+   * @param depositDate the deposit date of an object
+   */
+  @Override
+  public void setDepositDate(Date depositDate) {
+
+  }
+
+  /**
+   * Return the selling date of an object.
+   *
+   * @return a Date corresponding to the selling date of an object
+   */
+  @Override
+  public Date getSellingDate() {
+    return null;
+  }
+
+  /**
+   * set the selling date of an object.
+   *
+   * @param sellingDate the selling date of an object
+   */
+  @Override
+  public void setSellingDate(Date sellingDate) {
+
+  }
+
+  /**
+   * Return the withdrawal date of an object.
+   *
+   * @return a Date corresponding to the withdrawal date of an object
+   */
+  @Override
+  public Date getWithdrawalDate() {
+    return null;
+  }
+
+  /**
+   * set the withdrawal date of an object.
+   *
+   * @param withdrawalDate the withdrawal date of an object
+   */
+  @Override
+  public void setWithdrawalDate(Date withdrawalDate) {
+
+  }
+
+  /**
+   * Return the time slot of an object.
+   *
+   * @return a String corresponding to the time slot of an object
+   */
+  @Override
+  public String getTimeSlot() {
+    return null;
+  }
+
+  /**
+   * set the time slot of an object.
+   *
+   * @param timeSlot the time slot of an object
+   */
+  @Override
+  public void setTimeSlot(String timeSlot) {
+
+  }
+
+  /**
+   * Return the status of an object.
+   *
+   * @return a String corresponding to the status of an object
+   */
+  @Override
+  public String getStatus() {
+    return null;
+  }
+
+  /**
+   * set the status of an object.
+   *
+   * @param status the status of the object
+   */
+  @Override
+  public void setStatus(String status) {
+
+  }
+
+  /**
+   * Return the reason for refusal of an object.
+   *
+   * @return a String corresponding to the reason for refusal of an object
+   */
+  @Override
+  public String getReasonForRefusal() {
+    return null;
+  }
+
+  /**
+   * set the reason for refusal of an object.
+   *
+   * @param reasonForRefusal the reason for refusal of an object
+   */
+  @Override
+  public void setReasonForRefusal(String reasonForRefusal) {
+
+  }
+
+  /**
+   * Return the phone number of an object.
+   *
+   * @return a String corresponding to the phone number of an object
+   */
+  @Override
+  public String getPhoneNumber() {
+    return null;
+  }
+
+  /**
+   * set the phone number of an object.
+   *
+   * @param phoneNumber the phone number of an object
+   */
+  @Override
+  public void setPhoneNumber(String phoneNumber) {
+
+  }
+
+  /**
+   * Return the pickup date of an object.
+   *
+   * @return a date corresponding to the pickup date of an object
+   */
+  @Override
+  public Date getPickupDate() {
+    return null;
+  }
+
+  /**
+   * set the pickup date of an object.
+   *
+   * @param pickupDate the pickup date of an object
+   */
+  @Override
+  public void setPickupDate(Date pickupDate) {
+
+  }
+
+  /**
+   * Return the user of an object.
+   *
+   * @return a UserDTO corresponding to the user of an object
+   */
+  @Override
+  public UserDTO getUser() {
+    return null;
+  }
+
+  /**
+   * set the user of an object.
+   *
+   * @param user the user of an object
+   */
+  @Override
+  public void setUser(UserDTO user) {
+
+  }
+
+  /**
+   * Return the type of the object.
+   *
+   * @return a String corresponding to the type of the object
+   */
+  @Override
+  public String getObjectType() {
+    return null;
+  }
+
+  /**
+   * set the type of the object.
+   *
+   * @param objectType the type of the object
+   */
+  @Override
+  public void setObjectType(String objectType) {
+
+  }
+}
