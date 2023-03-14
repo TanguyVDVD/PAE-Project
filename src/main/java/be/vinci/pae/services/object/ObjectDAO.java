6686048c89b50c84ef50e1b0cbc8b74cedec5a6e
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-03-14 10:33:27 +0100

    feat: add getAll in ObjectDAO

--- src/main/java/be/vinci/pae/services/object/ObjectDAO.java
+++ src/main/java/be/vinci/pae/services/object/ObjectDAO.java
@@ -1,8 +1,18 @@
 package be.vinci.pae.services.object;
 
+import be.vinci.pae.domain.object.ObjectDTO;
+import java.util.List;
+
 /**
  * ObjectDAO interface that provide the method to interact with the db.
  */
 public interface ObjectDAO {
 
+  /**
+   * Get all objects.
+   *
+   * @param query query to filter objects
+   * @return the list of objects
+   */
+  List<ObjectDTO> getAll(String query);
 }
--- src/main/java/be/vinci/pae/services/object/ObjectDAOImpl.java
+++ src/main/java/be/vinci/pae/services/object/ObjectDAOImpl.java
@@ -1,8 +1,30 @@
 package be.vinci.pae.services.object;
 
+import be.vinci.pae.domain.DomainFactory;
+import be.vinci.pae.domain.object.ObjectDTO;
+import be.vinci.pae.services.DALServices;
+import jakarta.inject.Inject;
+import java.util.List;
+
 /**
  * ObjectDAO class that implements ObjectDAO interface Provide the different methods.
  */
 public class ObjectDAOImpl implements ObjectDAO {
 
+  @Inject
+  private DomainFactory myDomainFactory;
+
+  @Inject
+  private DALServices myDALServices;
+
+  /**
+   * Get all objects.
+   *
+   * @param query query to filter objects
+   * @return the list of objects
+   */
+  @Override
+  public List<ObjectDTO> getAll(String query) {
+    return null;
+  }
 }
