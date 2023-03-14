70d41a5072bba49a8fc9a780af4c796a52293609
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-03-14 12:19:44 +0100

    feat: add AvailabilityDTO and Availability

---
+++ src/main/java/be/vinci/pae/domain/availability/Availability.java
@@ -0,0 +1,8 @@
+package be.vinci.pae.domain.availability;
+
+/**
+ * Availability interface representing an availability in the domain.
+ */
+public interface Availability extends AvailabilityDTO {
+
+}
