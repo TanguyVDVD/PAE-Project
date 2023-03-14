fad3b6836c1b81f8b9e6b38e95cbe8fc94276e20
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-03-14 12:42:17 +0100

    feat: implement DomainFactory

--- src/main/java/be/vinci/pae/domain/DomainFactoryImpl.java
+++ src/main/java/be/vinci/pae/domain/DomainFactoryImpl.java
@@ -1,7 +1,11 @@
 package be.vinci.pae.domain;
 
+import be.vinci.pae.domain.availability.AvailabilityDTO;
+import be.vinci.pae.domain.availability.AvailabilityImpl;
 import be.vinci.pae.domain.object.ObjectDTO;
 import be.vinci.pae.domain.object.ObjectImpl;
+import be.vinci.pae.domain.objectType.ObjectTypeDTO;
+import be.vinci.pae.domain.objectType.ObjectTypeImpl;
 import be.vinci.pae.domain.user.UserDTO;
 import be.vinci.pae.domain.user.UserImpl;
 
@@ -28,6 +32,26 @@ public class DomainFactoryImpl implements DomainFactory {
   @Override
   public ObjectDTO getObject() {
     return new ObjectImpl();
+  }
+
+  /**
+   * Returns a new instance of a ObjectTypeDTO.
+   *
+   * @return an ObjectTypeImpl
+   */
+  @Override
+  public ObjectTypeDTO getObjectType() {
+    return new ObjectTypeImpl();
+  }
+
+  /**
+   * Returns a new instance of an AvailabilityDTO.
+   *
+   * @return a AvailabilityImpl
+   */
+  @Override
+  public AvailabilityDTO getAvailability() {
+    return new AvailabilityImpl();
   }
 
 }
