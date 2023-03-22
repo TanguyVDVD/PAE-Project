939bc6179e4fcda4fa908a45ec63bcde22f73878
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-03-22 10:58:52 +0100

    feat: add getOne in ObjectUCC

--- src/main/java/be/vinci/pae/ucc/object/ObjectUCC.java
+++ src/main/java/be/vinci/pae/ucc/object/ObjectUCC.java
@@ -17,6 +17,14 @@ public interface ObjectUCC {
   List<ObjectDTO> getObjects(String query);
 
   /**
+   * Returns an object corresponding to the id.
+   *
+   * @param id the id of the object to filter objects
+   * @return an object
+   */
+  ObjectDTO getOne(int id);
+
+  /**
    * Accept an offer.
    *
    * @param id the id of the object to accept
