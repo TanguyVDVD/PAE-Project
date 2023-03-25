38571cc00b9dc6475a8c9a687804578425948965
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-03-25 18:54:10 +0100

    feat: add ObjectTypeResource

---
+++ src/main/java/be/vinci/pae/api/ObjectTypeResource.java
@@ -0,0 +1,37 @@
+package be.vinci.pae.api;
+
+import be.vinci.pae.api.filters.AuthorizeAdmin;
+import be.vinci.pae.ucc.objecttype.ObjectTypeUCC;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+
+/**
+ * ObjectTypeResource class.
+ */
+@Singleton
+@Path("/objectsTypes")
+public class ObjectTypeResource {
+
+  private final ObjectMapper jsonMapper = new ObjectMapper();
+  @Inject
+  private ObjectTypeUCC objectTypeUCC;
+
+  /**
+   * Get a list of all object types.
+   *
+   * @return a list of object types
+   */
+  @GET
+  @AuthorizeAdmin
+  @Produces(MediaType.APPLICATION_JSON)
+  public ArrayNode getObjectTypes() {
+    return jsonMapper.valueToTree(objectTypeUCC.getObjectTypes());
+  }
+}
