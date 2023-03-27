122c855af7b943f2970b7ad55679ec665b61748b
Erman Sayin <erman.sayin@student.vinci.be>
2023-03-27 16:35:13 +0200

    feat: implement object photo upload

--- webapp/src/Components/Pages/ObjectPage.js
+++ webapp/src/Components/Pages/ObjectPage.js
@@ -1,12 +1,13 @@
-import {clearPage} from '../../utils/render';
-import {getAuthenticatedUser} from '../../utils/auths';
-import API from "../../utils/api";
-import {dateStringtoGoodFormat, getTodaySDate} from "../../utils/dates";
-import Navigate from "../Router/Navigate";
-import AdminOffersPage from "./admin/AdminOffersPage";
-import AdminObjectsPage from "./admin/AdminObjectsPage";
-import setUserOrPhoneNumber from "../../utils/objects";
+import { clearPage, renderError } from '../../utils/render';
+import { getAuthenticatedUser } from '../../utils/auths';
+import API from '../../utils/api';
+import { dateStringtoGoodFormat, getTodaySDate } from '../../utils/dates';
+import Navigate from '../Router/Navigate';
+import AdminOffersPage from './admin/AdminOffersPage';
+import AdminObjectsPage from './admin/AdminObjectsPage';
+import setUserOrPhoneNumber from '../../utils/objects';
 
+import noFurniturePhoto from '../../img/no_furniture_photo.svg';
 
 const ObjectPage = (params) => {
   const id = parseInt(params.id, 10);
@@ -14,31 +15,38 @@ const ObjectPage = (params) => {
   clearPage();
 
   API.get(`/objects/${id}`).then((object) => {
-    API.get("/objectsTypes").then((objectTypes) => {
+    API.get('/objectsTypes').then((objectTypes) => {
       renderObjectPage(object, objectTypes);
-    })
-  })
+    });
+  });
 };
 
 function renderObjectPage(object, objectTypes) {
-    const main = document.querySelector('main');
+  const main = document.querySelector('main');
 
-    const authenticatedUser = getAuthenticatedUser();
+  const authenticatedUser = getAuthenticatedUser();
 
-    const div = document.createElement('div');
+  const div = document.createElement('div');
 
-    div.className='container p-5'
+  div.className = 'container p-5';
 
-    div.innerHTML=`
+  div.innerHTML = `
     <section>
       <div>
           <div class="row gx-6 gx-lg-6 align-items-top">
-          
               <div class="col-md-4">
-                <img class="card-img-top mb-5 mb-md-0" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqsL6QorN-b6YhpcfTl9YJEzWB2xSkhFkN4Q&usqp=CAU" alt="..." />
+                <img class="card-img-top mb-5 mb-md-0 object-fit-cover" 
+                src="${
+                  object.photo ? API.getEndpoint(`objects/${object.id}/photo`) : noFurniturePhoto
+                }"
+                onerror="this.src='${noFurniturePhoto}'"
+                width="400"
+                height="400"
+                 />
                 
-                ${authenticatedUser && authenticatedUser.isHelper && object.state !== "proposé" ? 
-                  `
+                ${
+                  authenticatedUser && authenticatedUser.isHelper && object.state !== 'proposé'
+                    ? `
                     <form>
                       <div class="row row-cols-1 row-cols-md-2 m-1">
                         <input
@@ -51,30 +59,35 @@ function renderObjectPage(object, objectTypes) {
                         />
                       </div>
                     </form>
-                  ` : ''
+                  `
+                    : ''
                 }
               </div>
               
 
               <div class="col-md-8">
                   
-                  ${authenticatedUser && authenticatedUser.isHelper && object.state !== "proposé" ?
-                    `
+                  ${
+                    authenticatedUser && authenticatedUser.isHelper && object.state !== 'proposé'
+                      ? `
                       <form id="object-form">
                         <div class="form-group" id="object-type-form">
                           <label>Type</label>
                           <select class="form-control" id="object-type-select">
-                            ${objectTypes.map((objectType) =>
-                              `
+                            ${objectTypes.map(
+                              (objectType) =>
+                                `
                                 <option>${objectType.label}</option>
-                              `
+                              `,
                             )}
                           </select>
                         </div>
                         <br>
                         <div class="form-group" id="object-description-form">
                           <label>Description</label>
-                          <textarea class="form-control" id="object-description-textarea" rows="2">${object.description}</textarea>
+                          <textarea class="form-control" id="object-description-textarea" rows="2">${
+                            object.description
+                          }</textarea>
                         </div>
                         <br>
                         <div class="form-group div-user" id="object-user-form">
@@ -127,15 +140,16 @@ function renderObjectPage(object, objectTypes) {
                         <div class="bordure_verticale"></div>
                         <button type="submit" class="btn btn-outline-primary" id="cancel-btn">Annuler</button>
                       </form> 
-                    ` :
                     `
+                      : `
                       <h2>${object.objectType}</h2>
                       <p>Description : ${object.description}</p>
                     `
                   }
                   
-                  ${authenticatedUser && authenticatedUser.isHelper && object.state === "proposé" ?
-                    `
+                  ${
+                    authenticatedUser && authenticatedUser.isHelper && object.state === 'proposé'
+                      ? `
                       <div class="div-user" id="object-user-offer"></div>
                       
                       <div class="form-group" id="object-receipt-date-offer">
@@ -154,11 +168,13 @@ function renderObjectPage(object, objectTypes) {
                       <a href="#" class="accept" id="accept-btn">Accepter l'objet<span class="fa fa-check"></span></a>
                       <div class="bordure_verticale"></div>
                       <a href="#" class="deny" id="deny-btn">Refuser l'objet<span class="fa fa-close"></span></a>
-                    ` : ''
+                    `
+                      : ''
                   }
                   
-                  ${authenticatedUser && !authenticatedUser.isHelper ?
-                    `
+                  ${
+                    authenticatedUser && !authenticatedUser.isHelper
+                      ? `
                       <div id="object-state-non-helper">
                         <p>État : ${object.state}</p>
                       </div>
@@ -166,95 +182,118 @@ function renderObjectPage(object, objectTypes) {
                       <div id="object-price-non-helper">
                         <p>Prix : ${object.price}€</p>
                       </div>
-                    ` : ''
+                    `
+                      : ''
                   }
               </div>
           </div>
       </section>
     `;
 
-    main.appendChild(div);
+  main.appendChild(div);
 
-    if (authenticatedUser && authenticatedUser.isHelper && object.state === "proposé"){
-      const acceptBtn = document.getElementById("accept-btn");
-      const denyBtn = document.getElementById("deny-btn");
+  if (authenticatedUser && authenticatedUser.isHelper && object.state === 'proposé') {
+    const acceptBtn = document.getElementById('accept-btn');
+    const denyBtn = document.getElementById('deny-btn');
 
-      if (authenticatedUser.id !== 1){
-        acceptBtn.disabled = true;
-        denyBtn.disabled = true;
+    if (authenticatedUser.id !== 1) {
+      acceptBtn.disabled = true;
+      denyBtn.disabled = true;
+    }
+
+    acceptBtn.addEventListener('click', () => {
+      const status = 'accepté';
+
+      API.patch(`objects/status/${object.id}`, { body: { status } });
+      AdminOffersPage();
+      Navigate('/admin/offers');
+    });
+
+    denyBtn.addEventListener('click', () => {
+      const status = 'refusé';
+      const reasonForRefusal = document.getElementById('reason-for-refusal').value;
+
+      API.patch(`objects/status/${object.id}`, { body: { status, reasonForRefusal } });
+      AdminOffersPage();
+      Navigate('/admin/offers');
+    });
+  }
+
+  if (authenticatedUser && authenticatedUser.isHelper && object.state !== 'proposé') {
+    setUserOrPhoneNumber(document, 'div-user', [object]);
+    setDefaultValues(object);
+
+    const stateForm = document.getElementById('object-state-select');
+    const priceInput = document.getElementById('object-price-input');
+
+    stateForm.addEventListener('change', () => {
+      if (
+        stateForm.value === 'accepté' ||
+        stateForm.value === "à l'atelier" ||
+        stateForm.value === 'en magasin'
+      ) {
+        priceInput.value = null;
+        priceInput.disabled = true;
+      } else if (stateForm.value === 'retiré') {
+        priceInput.value = object.price;
+        priceInput.disabled = true;
+      } else {
+        priceInput.value = object.price;
+        priceInput.disabled = false;
       }
+    });
 
-      acceptBtn.addEventListener('click', () => {
-        const status = "accepté";
+    document.getElementById('object-form').addEventListener('submit', async (e) => {
+      e.preventDefault();
 
-        API.patch(`objects/status/${object.id}`, {body: {status}});
-        AdminOffersPage();
-        Navigate('/admin/offers');
-      });
+      const description = document.getElementById('object-description-textarea').value;
+      const type = document.getElementById('object-type-select').value;
+      const state = document.getElementById('object-state-select').value;
+      const date = document.getElementById('object-state-date-input').value;
+      const price = document.getElementById('object-price-input').value;
+      const isVisible = document.getElementById('visible-switch').checked;
 
-      denyBtn.addEventListener('click', () => {
-        const status = "refusé";
-        const reasonForRefusal = document.getElementById("reason-for-refusal").value;
+      try {
+        await API.put(`objects/${object.id}`, {
+          body: { description, type, state, date, price, isVisible },
+        });
 
-        API.patch(`objects/status/${object.id}`, {body: {status, reasonForRefusal}});
-        AdminOffersPage();
-        Navigate('/admin/offers');
-      });
-
-    }
-
-    if (authenticatedUser && authenticatedUser.isHelper && object.state !== "proposé"){
-      setUserOrPhoneNumber(document, "div-user", [object]);
-      setDefaultValues(object);
-
-      const stateForm = document.getElementById("object-state-select");
-      const priceInput = document.getElementById("object-price-input");
-
-      stateForm.addEventListener("change", () => {
-        if (stateForm.value === "accepté" || stateForm.value === "à l'atelier" || stateForm.value === "en magasin"){
-          priceInput.value = null;
-          priceInput.disabled = true;
-        } else if (stateForm.value === "retiré"){
-          priceInput.value = object.price;
-          priceInput.disabled = true;
-        } else {
-          priceInput.value = object.price;
-          priceInput.disabled = false;
+        const photo = document.getElementById('input-photo');
+        if (photo.files.length > 0) {
+          const formData = new FormData();
+          formData.append('photo', photo.files[0]);
+          await API.put(`objects/${object.id}/photo`, { body: formData });
         }
-      });
 
-      document.getElementById("save-btn").addEventListener('click', () => {
-        const description = document.getElementById("object-description-textarea").value;
-        const type = document.getElementById("object-type-select").value;
-        const state = document.getElementById("object-state-select").value;
-        const date = document.getElementById("object-state-date-input").value;
-        const price = document.getElementById("object-price-input").value;
-        const isVisible = document.getElementById("visible-switch").checked;
-
-        API.put(`objects/${object.id}`, {body: {description, type, state, date, price, isVisible}});
         AdminObjectsPage();
         Navigate('/admin/objects');
-      });
+      } catch (error) {
+        renderError(error);
+      }
+    });
 
-      document.getElementById("cancel-btn").addEventListener('click', () => {
-        AdminObjectsPage();
-        Navigate('/admin/objects');
-      });
-    }
-
+    document.getElementById('cancel-btn').addEventListener('click', () => {
+      AdminObjectsPage();
+      Navigate('/admin/objects');
+    });
+  }
 }
 
-function setDefaultValues(object){
-  document.getElementById("object-type-select").value = object.objectType;
-  document.getElementById("object-state-select").value = object.state;
-  document.getElementById("object-state-date-input").value = getTodaySDate();
+function setDefaultValues(object) {
+  document.getElementById('object-type-select').value = object.objectType;
+  document.getElementById('object-state-select').value = object.state;
+  document.getElementById('object-state-date-input').value = getTodaySDate();
 
-  const priceInput = document.getElementById("object-price-input");
+  const priceInput = document.getElementById('object-price-input');
 
-  if (object.state === "accepté" || object.state === "à l'atelier" || object.state === "en magasin"){
+  if (
+    object.state === 'accepté' ||
+    object.state === "à l'atelier" ||
+    object.state === 'en magasin'
+  ) {
     priceInput.value = null;
     priceInput.disabled = true;
-  } else if (object.state === "retiré") {
+  } else if (object.state === 'retiré') {
     priceInput.value = object.price;
     priceInput.disabled = true;
   } else {
@@ -262,7 +301,7 @@ function setDefaultValues(object){
     priceInput.disabled = false;
   }
 
-  const visibleSwitch = document.getElementById("visible-switch");
+  const visibleSwitch = document.getElementById('visible-switch');
 
   visibleSwitch.checked = !!object.isVisible;
 }
