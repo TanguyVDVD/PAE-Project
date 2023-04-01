08424f40e94970e73aa01a1174b7ce7f36af1b2b
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-04-01 23:32:27 +0200

    feat: add getAvailabilities in AvailabilityUCC

--- src/main/java/be/vinci/pae/ucc/availability/AvailabilityUCC.java
+++ src/main/java/be/vinci/pae/ucc/availability/AvailabilityUCC.java
@@ -1,8 +1,18 @@
 package be.vinci.pae.ucc.availability;
 
+import be.vinci.pae.domain.availability.AvailabilityDTO;
+import java.util.List;
+
 /**
  * AvailabilityUCC interface that provide the methods of an availability.
  */
 public interface AvailabilityUCC {
+
+  /**
+   * Returns a list of all availabilities.
+   *
+   * @return a list of all availabilities
+   */
+  List<AvailabilityDTO> getAvailabilities();
 
 }
--- src/main/java/be/vinci/pae/ucc/availability/AvailabilityUCCImpl.java
+++ src/main/java/be/vinci/pae/ucc/availability/AvailabilityUCCImpl.java
@@ -1,8 +1,45 @@
 package be.vinci.pae.ucc.availability;
 
+import be.vinci.pae.domain.availability.AvailabilityDTO;
+import be.vinci.pae.services.DALServices;
+import be.vinci.pae.services.availability.AvailabilityDAO;
+import be.vinci.pae.utils.MyLogger;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response.Status;
+import java.util.List;
+import java.util.logging.Level;
+
 /**
  * AvailabilityUCCImpl class that implements the AvailabilityUCC interface.
  */
 public class AvailabilityUCCImpl implements AvailabilityUCC {
 
+  @Inject
+  private AvailabilityDAO myAvailabilityDAO;
+
+  @Inject
+  private DALServices myDalServices;
+
+  /**
+   * Returns a list of all availabilities.
+   *
+   * @return a list of all availabilities
+   */
+  @Override
+  public List<AvailabilityDTO> getAvailabilities() {
+    myDalServices.startTransaction();
+    try {
+      return myAvailabilityDAO.getAll();
+    } catch (Exception e) {
+      myDalServices.rollbackTransaction();
+      MyLogger.log(Level.INFO,
+          "Erreur lors de la récupération de la liste des disponibilités");
+      throw new WebApplicationException(
+          "Erreur lors de la récupération de la liste des disponibilités",
+          Status.INTERNAL_SERVER_ERROR);
+    } finally {
+      myDalServices.commitTransaction();
+    }
+  }
 }
