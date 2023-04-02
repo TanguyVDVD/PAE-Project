98cd075c246d3c7c449786d7e53021e1c6429af1
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-04-02 21:05:17 +0200

    feat: add AdminAvailabilitiesPage

---
+++ webapp/src/Components/Pages/admin/AdminAvailabilitiesPage.js
@@ -0,0 +1,41 @@
+import {getAuthenticatedUser} from "../../../utils/auths";
+import Navigate from "../../Router/Navigate";
+import {clearPage} from "../../../utils/render";
+
+const AdminAvailabilitiesPage = () => {
+  const authenticatedUser = getAuthenticatedUser();
+
+  if (!authenticatedUser || authenticatedUser.role === null) {
+    Navigate('/');
+    return;
+  }
+
+  clearPage();
+  renderAdminAvailabilitiesPage();
+  renderDatePicker();
+};
+
+function renderAdminAvailabilitiesPage() {
+  const main = document.querySelector('main');
+  const div = document.createElement('div');
+  div.className = 'container my-5';
+
+  div.innerHTML = `
+    <h2>Disponibilités</h2>
+    <div id="div-date-picker"></div>
+  `;
+
+  main.appendChild(div);
+}
+
+function renderDatePicker() {
+  const main = document.querySelector('main');
+
+  const authenticatedUser = getAuthenticatedUser();
+
+  const div = document.createElement('div');
+
+  div.className = 'container p-5';
+
+  div.innerHTML = ``;
+}
