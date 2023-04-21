918106cdf66f052715d254e0dc0ffc09b53d9c90
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-04-21 17:54:55 +0200

    feat: add add method in ObjectResource

--- src/main/java/be/vinci/pae/api/ObjectResource.java
+++ src/main/java/be/vinci/pae/api/ObjectResource.java
@@ -6,6 +6,7 @@ import be.vinci.pae.api.filters.AuthorizeManager;
 import be.vinci.pae.domain.DomainFactory;
 import be.vinci.pae.domain.object.ObjectDTO;
 import be.vinci.pae.domain.user.User;
+import be.vinci.pae.domain.user.UserDTO;
 import be.vinci.pae.ucc.object.ObjectUCC;
 import be.vinci.pae.utils.MyObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -17,6 +18,7 @@ import jakarta.inject.Singleton;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -278,5 +280,81 @@ public class ObjectResource {
     }
 
     return objectAfterUpdate;
+  }
+
+  /**
+   * Offer an object.
+   *
+   * @param request     the request
+   * @param description the description of the object
+   * @param objectType  the object type
+   * @param receiptDate the day to receipt the object
+   * @param timeSlot    the time slot chosen
+   * @param phoneNumber the phone number used to offer the object
+   * @param photo       the photo of the user
+   * @param photoDetail the detail of the photo
+   * @return an object when added
+   */
+  @Consumes(MediaType.MULTIPART_FORM_DATA)
+  @Produces(MediaType.APPLICATION_JSON)
+  @POST
+  public ObjectDTO add(
+      @Context ContainerRequest request,
+      @FormDataParam("phoneNumber") String phoneNumber,
+      @FormDataParam("description") String description,
+      @FormDataParam("objectType") String objectType,
+      @FormDataParam("receiptDate") LocalDate receiptDate,
+      @FormDataParam("timeSlot") String timeSlot,
+      @FormDataParam("photo") InputStream photo,
+      @FormDataParam("photo") FormDataContentDisposition photoDetail
+  ) {
+    UserDTO authorizedUser = (UserDTO) request.getProperty("user");
+
+    // Check the user
+    if (authorizedUser == null) {
+      if (phoneNumber == null || phoneNumber.isBlank()) {
+        throw new WebApplicationException(
+            "Numéro de téléphonne manquant ou utilisateur non connecté",
+            Response.Status.BAD_REQUEST);
+      } else {
+        // Check phone number format
+        phoneNumber = User.formatPhoneNumber(phoneNumber);
+        if (phoneNumber == null) {
+          throw new WebApplicationException("Numéro de téléphone invalide",
+              Response.Status.BAD_REQUEST);
+        }
+      }
+    } else if (phoneNumber != null && !phoneNumber.isBlank()) {
+      throw new WebApplicationException(
+          "Impossible de proser un objet anonymement pour un utilisateur connecté",
+          Response.Status.BAD_REQUEST);
+    }
+
+    String[] inputs = {description, objectType, timeSlot};
+
+    for (String input : inputs) {
+      if (input == null || input.isBlank()) {
+        throw new WebApplicationException("Paramètres manquants", Response.Status.UNAUTHORIZED);
+      }
+    }
+
+    ObjectDTO objectDTO = myDomainFactory.getObject();
+
+    objectDTO.setDescription(description);
+    objectDTO.setObjectType(objectType);
+    objectDTO.setReceiptDate(receiptDate);
+    objectDTO.setTimeSlot(timeSlot);
+    objectDTO.setPhoneNumber(phoneNumber);
+    objectDTO.setUser(authorizedUser);
+    objectDTO.setPhoto(photoDetail != null && photoDetail.getFileName() != null);
+    objectDTO.setOfferDate(LocalDate.now());
+
+    ObjectDTO objectAdded = objectUCC.add(objectDTO);
+
+    if (objectAdded.getPhoto()) {
+      objectUCC.updatePhoto(objectAdded, photo);
+    }
+
+    return objectAdded;
   }
 }
