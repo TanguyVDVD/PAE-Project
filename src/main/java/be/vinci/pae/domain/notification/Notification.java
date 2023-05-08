d5d827c68c262450175122127572aceca15898f8
Tanguy Vandervelden <tanguy.vandervelden@student.vinci.be>
2023-05-08 16:50:15 +0200

    refactor : notifications

--- src/main/java/be/vinci/pae/domain/notification/Notification.java
+++ src/main/java/be/vinci/pae/domain/notification/Notification.java
@@ -1,22 +1,10 @@
 package be.vinci.pae.domain.notification;
 
 
-import be.vinci.pae.domain.object.ObjectDTO;
-
 /**
  * Notification interface representing a notification in the domain.
  */
 public interface Notification extends NotificationDTO {
-
-
-  /**
-   * Set up the notification text based on the object.
-   *
-   * @param objectDTO       the object the notification is from
-   * @param notificationDTO the notification to set up
-   * @return the notification when all attributes are set
-   */
-  NotificationDTO setUpNotificationText(ObjectDTO objectDTO, NotificationDTO notificationDTO);
 
   /**
    * Set up the notification based on the user.
